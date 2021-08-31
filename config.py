f5f467c122ee14b957f976ccccfdfbcab3a8ea8f
Genry665 <genry6651@gmail.com>
2021-08-31 20:56:31 +0300

    Revert "Delete config.py"
    This reverts commit 6c343b64930b1040ab6b33b41ef9c6ac1fc2ecd9.

---
+++ config.py
@@ -0,0 +1,7 @@
+
+API_TOKEN = '1900402677:AAG4C6NKCpbAOAM3XCY8x3vSQwL8JotIwTM'
+
+Msr_An = 807478887
+Mr_Sir = 756977672
+
+list_expense = ['коты', 'корм', 'туалет', 'наполнитель', 'еда', 'cat', '-']
